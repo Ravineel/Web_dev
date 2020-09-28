0936500819b510cdd2a210414f224163475e2754
Ravineel <rajatsinghi89@gmail.com>
2020-09-28 20:54:39 +0530

    js

---
+++ Training/JS/first/sript.js
@@ -0,0 +1,22 @@
+if(4+3===7){
+    console.log("Helloooo");
+}
+
+function sayHello(){
+    console.log("Hello");
+}
+
+sayHello();
+
+var sayBye = function(){
+    console.log("Bye");
+}
+sayBye();
+
+function sing( song ){
+    console.log(song);
+}
+
+sing("laaa deee daaa");
+sing("Hellooooo");
+sing("Blah blah blah")
