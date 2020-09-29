49bd23c1757eafcb77bc1dba4abac82c48a4002e
Ravineel <rajatsinghi89@gmail.com>
2020-09-29 21:06:03 +0530

    domjs

---
+++ Training/domjs/script.js
@@ -0,0 +1,10 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+button.addEventListener("click", function() {
+
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode("testing"))
+    ul.appendChild(li);
+})
