d6c3ea8af8b8ab1a782eee742e2572f913080324
Ravineel <rajatsinghi89@gmail.com>
2020-09-29 21:37:42 +0530

    js todo

--- Training/domjs/script.js
+++ Training/domjs/script.js
@@ -2,9 +2,29 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-button.addEventListener("click", function() {
+function inputLength(){
+    return input.value.length
+}
 
+function createListElement(){
     var li = document.createElement("li");
-    li.appendChild(document.createTextNode("testing"))
+    li.appendChild(document.createTextNode(input.value))
     ul.appendChild(li);
+    input.value="";
+}
+
+button.addEventListener("click", function() {
+    if (inputLength()>0){
+        createListElement();
+    }
 })
+
+input.addEventListener("keypress", function() {
+
+    if (inputLength()>0 && event.keyCode===13){
+        createListElement();    
+    }
+})
+
+
+
