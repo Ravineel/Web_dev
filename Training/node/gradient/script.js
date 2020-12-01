ed6c97e6fb2646eb87768dcf0ba21fbb06e57cd2
Ravineel <rajatsinghi89@gmail.com>
2020-12-01 10:31:35 +0530

    portfolio

---
+++ Training/node/gradient/script.js
@@ -0,0 +1,27 @@
+// import { without } from 'lodash';
+
+// console.log(without)
+
+ var _ =require('lodash');
+ console.log(_);
+
+
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
