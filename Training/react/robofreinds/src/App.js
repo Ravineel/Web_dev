ed299040e5a5cee73c951f2aca8c5eedc1d7e9b5
Ravineel <rajatsinghi89@gmail.com>
2020-12-02 22:49:12 +0530

    robofriends

---
+++ Training/react/robofreinds/src/App.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import './App.css'
+class App extends Component {
+
+    constructor(){ //smartcomponent
+        super();
+        this.state={
+            robots : [],
+            searchfield:''
+        }
+    }
+    
+    
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then( response => response.json())
+        .then( users =>this.setState({robots:users}));
+        
+    }
+    
+    
+    onSearchChange = (event) =>{    
+        this.setState({searchfield: event.target.value})
+    }
+
+    render(){ 
+        const filteredRobots = this.state.robots.filter(robots=>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+
+        if(this.state.robots.legth === 0){
+
+            return <h1>Loading</h1>
+
+        }
+        
+        else{
+            return (
+                <div className='tc'>
+                    <h1 className='f2'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}  />
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
+    }
+
+
+
+}
+
+export default App;
