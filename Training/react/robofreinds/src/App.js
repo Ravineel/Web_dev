e1fcc83ed9868415398cde8b43b951c22c86d1e8
Ravineel <rajatsinghi89@gmail.com>
2020-12-02 22:58:48 +0530

    scroll

--- Training/react/robofreinds/src/App.js
+++ Training/react/robofreinds/src/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css'
+import Scroll from './Scroll';
+
 class App extends Component {
 
     constructor(){ //smartcomponent
@@ -41,7 +43,9 @@ class App extends Component {
                 <div className='tc'>
                     <h1 className='f2'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}  />
-                    <CardList robots={filteredRobots}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
                 </div>
             );
         }
