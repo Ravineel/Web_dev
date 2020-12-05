f1c97bc43f6c05705b2259b045a133244a2888fa
Ravineel <rajatsinghi89@gmail.com>
2020-12-05 23:39:17 +0530

    nodejs server

---
+++ Training/node/nodejs/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const server = http.createServer((request,response)=>{
+    
+    console.log('method ', request.method)
+    console.log('url ', request.url)
+
+    const user = {
+        name:'John',
+        hobby:'Skatting'
+    }
+
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify(user));
+
+})
+
+server.listen(2000);
