e91963e3e897f10fc29a6cbefcb8fde9e49bef89
Ravineel <rajatsinghi89@gmail.com>
2020-12-06 15:13:29 +0530

    yeet

--- Training/node/nodejs/server.js
+++ Training/node/nodejs/server.js
@@ -1,18 +1,41 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((request,response)=>{
+// const server = http.createServer((request,response)=>{
     
-    console.log('method ', request.method)
-    console.log('url ', request.url)
+//     console.log('method ', request.method)
+//     console.log('url ', request.url)
 
-    const user = {
-        name:'John',
-        hobby:'Skatting'
-    }
+//     const user = {
+//         name:'John',
+//         hobby:'Skatting'
+//     }
 
-    response.setHeader('Content-Type', 'application/json');
-    response.end(JSON.stringify(user));
+//     response.setHeader('Content-Type', 'application/json');
+//     response.end(JSON.stringify(user));
 
-})
+// })
 
-server.listen(2000);
+// server.listen(2000);
+
+
+const express = require('express');
+
+const app = express()
+
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+
+app.get('/:id',(req,res)=>{
+
+    // console.log(req.query);
+    // req.body
+    // req.headers
+    console.log(req.params)
+    res.status(404).send("not found ");
+
+});
+
+
+
+app.listen(8080);
