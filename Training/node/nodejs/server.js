9b7530edb6c9fa29314fcb70c90c1f2b565bda1d
Ravineel <rajatsinghi89@gmail.com>
2020-12-06 15:18:19 +0530

    yy

--- Training/node/nodejs/server.js
+++ Training/node/nodejs/server.js
@@ -22,20 +22,7 @@ const express = require('express');
 
 const app = express()
 
-
-app.use(express.urlencoded({extended:false}))
-app.use(express.json());
-
-app.get('/:id',(req,res)=>{
-
-    // console.log(req.query);
-    // req.body
-    // req.headers
-    console.log(req.params)
-    res.status(404).send("not found ");
-
-});
-
+app.use(express.static(__dirname +'/public'))
 
 
 app.listen(8080);
